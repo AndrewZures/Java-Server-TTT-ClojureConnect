3ab20f7afebfc24e65eabcfe6bf8f356768f5349
Andrew James Zures <andrew.zures@gmail.com>
2013-09-09 14:02:31 -0500

    updated dependencies to clojar dependencies

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cloj_server "0.1.0-SNAPSHOT"
+(defproject org.clojars.andrewzures/ruby_java_clojure_server "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
@@ -6,9 +6,9 @@
   :profiles {:dev {:dependencies [[speclj "2.7.0"]]}}
   :plugins [[speclj "2.7.0"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [java-server "0.1.6-SNAPSHOT"]
-                 [ruby_ttt "0.1.3-SNAPSHOT"]
-                 [jruby-lein "0.1.0-SNAPSHOT"]]
+                 [org.clojars.andrewzures/java-server "0.1.0-SNAPSHOT"]
+                 [org.clojars.andrewzures/ruby_ttt "0.1.0-SNAPSHOT"]
+                 [org.clojars.andrewzures/jruby-lein "0.1.0-SNAPSHOT"]]
   :main cloj-server.core
   :test-path "spec/"
   )
