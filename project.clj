fbd6b941afad5f224cced6a94cd71ff204692224
Andrew Zures <azures@8thlight.com>
2013-12-10 16:01:57 -0600

    updated project_clj file to compile java classes, removed idea file from git

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :profiles {:dev {:dependencies [[speclj "2.7.0"]]}}
-  :plugins [[speclj "2.7.0"]]
+  :profiles {:dev {:dependencies [[speclj "2.9.0"]]}}
+  :plugins [[speclj "2.9.0"]]
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojars.andrewzures/java-server "0.2.2-SNAPSHOT"]
                  [org.clojars.andrewzures/ruby_ttt "0.1.3-SNAPSHOT"]
                  [org.clojars.andrewzures/jruby-lein "0.1.0-SNAPSHOT"]]
   :main cloj-server.core
+  :java-source-paths ["src/tttmiddleware/interfaces"]
   :test-paths ["spec/"]
   )
